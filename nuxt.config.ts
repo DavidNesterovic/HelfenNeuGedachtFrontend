e2f4a578db0fa570d4e8a6943a0c4018842d7269
dave <david.nesterovic@hotmail.com>
2026-04-13 15:13:47 +0200

    tailwind + page fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['@/assets/css/styles.css']
+  css: ['@/assets/css/styles.css'],
+  modules: ['@nuxtjs/tailwindcss']
 })
